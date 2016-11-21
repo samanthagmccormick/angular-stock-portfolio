40359c2c7d65ea61727ec417f9e0bd21550524cd
fa7b53c24b72c491e28d4a4aef4c0169d3f1513e <samanthagmccormick@gmail.com>
2016-11-20 19:52:17 -0700

    Market and some of Investor views set up

--- src/app.js
+++ src/app.js
@@ -1,46 +1,95 @@
-(function () {
-   'use strict';
+(function() {
+  'use strict';
 }());
 
-//
-angular.module('app', [])
-  .controller('AppCtrl', ['$http',  function($http) {
-    var self = this;
-    self.myStock = {};
-    self.myInvestor = {};
+var app = angular.module('app', ['ui.router']);
 
-    // on success of GET of investors[0] endpoint
-    $http.get('/active-investor').success(function(investor) {
-      self.myInvestor = investor;
+// Internal routing
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
 
-      // DO STUFF
+    $stateProvider
+      .state('market', {
+        cache: false,
+        url: '/market',
+        templateUrl: '/market.html',
+        controller: 'MarketCtrl'
+      })
+
+    .state('investor', {
+      url: '/investor',
+      templateUrl: '/investor.html',
+      controller: 'InvestorCtrl'
+    })
+
+    .state('manager', {
+      url: '/manager',
+      templateUrl: '/manager.html',
+      controller: 'ManagerCtrl'
     });
 
-    // on refresh of app...
-    self.refresh = function() {
+
+    // Handle incorrectly entered routes, i.e. redirect to "home" state
+    $urlRouterProvider.otherwise('market');
+  }
+]);
+
+
+// creating a factory to store data from controller, better for testing, passing to other controllers, etc.
+app.factory('total', [function() {
+  var o = {
+      total: 0
+    }
+    // returning another variable that CONTAINS posts allows us to store more
+    // data in this factory in the future
+  return o;
+}]);
+
+// on load of webpage, load MarketCtrl
+app.controller('MarketCtrl', ['$scope', '$http', function($scope, $http) {
+    $scope.myStock = {};
+
+    $scope.loadStocks = function() {
+      $http.get('/stocks').success(function(response) {
+        $scope.stocks = response;
+      });
+    }
+
+    // on refresh of single stock...
+    $scope.refresh = function(stock, index) {
+      $scope.index = index;
+
       // on success of GET of stocks
       $http.get('/stocks').success(function(stocks) {
-        // set myStock to the very first stock you receive in the array
-        self.myStock = stocks[0];
+
+        // update the data of the stock that you clicked
+        $scope.stocks[$scope.index] = stocks[$scope.index];
 
         // Stock.tradeHistory = {prices: [], timestamps: []};
         // Map over the Stock tradeHistory.timestamps[] and store DATES instead
-        self.myStock.tradeHistory.timestamps = self.myStock.tradeHistory.timestamps.map(function(timestamp) {
+        $scope.stocks[$scope.index].tradeHistory.timestamps = stocks[$scope.index].tradeHistory.timestamps.map(function(timestamp) {
           return new Date(timestamp);
         });
 
+
         // chart.load = c3 (d3 helper) http://c3js.org/samples/data_load.html
-        self.chart.load({columns: [
-          // "price",1.00,2.00,3.00, etc.
-          ['price'].concat(self.myStock.tradeHistory.prices),
-          // "timestamp",10:00:00,11:00:01, etc.
-          ['timestamp'].concat(self.myStock.tradeHistory.timestamps)
-        ]});
+        $scope.chart.load({
+          columns: [
+            // "price",1.00,2.00,3.00, etc.
+            ['price'].concat($scope.stocks[$scope.index].tradeHistory.prices),
+            // "timestamp",10:00:00,11:00:01, etc.
+            ['timestamp'].concat($scope.stocks[$scope.index].tradeHistory.timestamps)
+          ]
+        });
+
       });
+
     };
 
     // an example chart
-    self.chartConfig = {
+    $scope.chartConfig = {
       bindto: '#stock-chart',
       data: {
         x: 'timestamp',
@@ -53,12 +102,88 @@ angular.module('app', [])
         x: {
           type: 'timeseries',
           tick: {
-            format: '%H:%M:%S'
+            format: '%H:%M:%S',
+            count: 25
           }
         }
       }
     };
 
-    self.chart = c3.generate(self.chartConfig);
-    self.refresh();
-  }]);
+    $scope.chart = c3.generate($scope.chartConfig);
+    // load stocks
+    $scope.loadStocks();
+  }]) // end MarketCtrl
+
+
+app.controller('InvestorCtrl', [
+  '$scope',
+  '$http',
+  'total', // inject "total" factory to use across controllers
+  function($scope, $http, total) {
+    $scope.total = total.total;
+
+    // Directives
+    $scope.loadInvestor = function() {
+      $scope.myInvestor = {};
+
+      // on success of GET of investors[0] endpoint
+      $http.get('/active-investor').success(function(response) {
+        console.log('investor...');
+        console.log(response);
+
+        $scope.myInvestor = response;
+      });
+    }
+
+    $scope.loadStocks = function() {
+      // on success of GET of investors[0] endpoint
+      $http.get('/stocks').success(function(response) {
+        console.log('stocks...');
+        console.log(response);
+
+        $scope.stocks = response;
+      });
+    }
+
+    $scope.changeQuote = function(quantity, index) {
+      $scope.index = index;
+
+      // price * quantity = subtotal
+      $scope.subtotal = $scope.stocks[$scope.index].price * quantity;;
+
+      // pass subtotal to grandTotal()
+      $scope.grandTotal($scope.subtotal);
+
+    }
+
+    $scope.grandTotal = function(total) {
+      $scope.total += total;
+
+      console.log("grandTotal");
+    }
+
+    $scope.getQuote = function() {
+      $http.get('stocks').success(function(response) {
+        console.log(response);
+      });
+    }
+
+    $scope.buyStock = function() {
+
+    }
+
+    $scope.loadInvestor();
+    $scope.loadStocks();
+  }]) // end InvestorCtrl
+
+
+
+app.controller('ManagerCtrl', [
+  '$scope',
+  function($scope) {
+    $scope.test = 'ManagerCtrl Test';
+
+    console.log('ManagerCtrl!');
+
+  }
+]); // end ManagerCtrl
