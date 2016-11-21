35e53595e52cb33553e0f526a222452c70afd69f
fa7b53c24b72c491e28d4a4aef4c0169d3f1513e <samanthagmccormick@gmail.com>
2016-11-20 22:12:11 -0700

    Adding ability to buy stock, form validation, etc.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
   'use strict';
 }());
 
-var app = angular.module('app', ['ui.router']);
+var app = angular.module('app', ['ui.router', 'ng-polymer-elements']);
 
 // Internal routing
 app.config([
@@ -12,7 +12,6 @@ app.config([
 
     $stateProvider
       .state('market', {
-        cache: false,
         url: '/market',
         templateUrl: '/market.html',
         controller: 'MarketCtrl'
@@ -116,65 +115,151 @@ app.controller('MarketCtrl', ['$scope', '$http', function($scope, $http) {
 
 
 app.controller('InvestorCtrl', [
-  '$scope',
-  '$http',
-  'total', // inject "total" factory to use across controllers
-  function($scope, $http, total) {
-    $scope.total = total.total;
+    '$scope',
+    '$http',
+    '$filter',
+    'total', // inject "total" factory to use across controllers
+    function($scope, $http, $filter, total) {
+      $scope.total = total.total;
 
-    // Directives
-    $scope.loadInvestor = function() {
-      $scope.myInvestor = {};
+      // check if market is open
+      var marketIsOpen;
+      $http.get('/marketStatus').success(function(response) {
+        marketIsOpen = response;
 
-      // on success of GET of investors[0] endpoint
-      $http.get('/active-investor').success(function(response) {
-        console.log('investor...');
-        console.log(response);
-
-        $scope.myInvestor = response;
+        if (marketIsOpen) {
+          $scope.marketStatus = 'Market is open';
+        } else {
+          $scope.marketStatus = 'Market is closed';
+        }
       });
+
+      // Directives
+      $scope.loadInvestor = function() {
+        $scope.myInvestor = {};
+
+        // on success of GET of investors[0] endpoint
+        $http.get('/active-investor').success(function(response) {
+          console.log('investor...');
+          console.log(response);
+
+          $scope.myInvestor = response;
+        });
+      }
+
+      $scope.loadStocks = function() {
+        // on success of GET of investors[0] endpoint
+        $http.get('/stocks').success(function(response) {
+          console.log('stocks...');
+          console.log(response);
+
+          $scope.stocks = response;
+        });
+      }
+
+      $scope.changeQuote = function(quantity, index) {
+        $scope.index = index;
+
+        // price * quantity = subtotal
+        $scope.subtotal = $scope.stocks[$scope.index].price * quantity;;
+
+        // pass subtotal to grandTotal()
+        $scope.grandTotal($scope.subtotal);
+
+      }
+
+      // FIXME: it is keeping a running total, need to store it locally
+      $scope.grandTotal = function(total) {
+        $scope.total += total;
+
+        console.log("grandTotal");
+      }
+
+      $scope.getQuote = function() {
+        $http.get('stocks').success(function(response) {
+          console.log(response);
+        });
+      }
+
+      $scope.buyStock = function(symbol, amount) {
+        // check if symbol is valid
+        if (symbol === '' || symbol === undefined || $filter('filter')($scope.stocks, {
+            symbol: symbol
+          })[0] === undefined) {
+          $scope.errorMessage("symbol");
+          return;
+        }
+
+        // check if amount is valid
+        if (amount === '' || amount === undefined || amount < 0) {
+          $scope.errorMessage("amount");
+          return;
+        }
+
+        // search for symbol in stocks array
+        var stock = $filter('filter')($scope.stocks, {
+          symbol: symbol
+        })[0];
+
+        // calculate total $$
+        var transactionTotal = stock.price * amount;
+
+        // check if total $$ exceeds the investor's capital
+        if (transactionTotal > $scope.myInvestor.capital) {
+          $scope.errorMessage("capital")
+          return;
+        }
+
+        console.log('all good');
+
+        // if the market is open, buy stock!
+        if (marketIsOpen) {
+          // Buy stock! (use Market function)
+          $http.get('/market', {
+              params: {
+                investorId: $scope.myInvestor.investorId,
+                symbol: symbol,
+                quoteId: 1, // TODO create real ID
+                quantity: amount
+              }
+            })
+            .success(function(response) {
+              console.log("HTTP GET completed");
+              console.log(response);
+
+            });
+        } else {
+          $scope.errorMessage("closed");
+        }
+      }
+
+      // TODO move to view (should not be in controller)
+      $scope.errorMessage = function(item) {
+        var message;
+
+        switch (item) {
+          case 'symbol':
+          case 'amount':
+            message = 'Sorry, you did not enter a valid ' + item + '. Please try again.';
+            break;
+          case 'capital':
+            message = 'Sorry, you do not have enough ' + item + ' to make this transaction.';
+            break;
+          case 'closed':
+            message = 'The market is closed. Your transaction did not go through.';
+            break;
+          default:
+            message = 'Oops, application error...no message found.';
+        }
+        console.log(message);
+
+        $scope.errorMessage = message;
+      }
+
+      $scope.loadInvestor();
+      $scope.loadStocks();
     }
-
-    $scope.loadStocks = function() {
-      // on success of GET of investors[0] endpoint
-      $http.get('/stocks').success(function(response) {
-        console.log('stocks...');
-        console.log(response);
-
-        $scope.stocks = response;
-      });
-    }
-
-    $scope.changeQuote = function(quantity, index) {
-      $scope.index = index;
-
-      // price * quantity = subtotal
-      $scope.subtotal = $scope.stocks[$scope.index].price * quantity;;
-
-      // pass subtotal to grandTotal()
-      $scope.grandTotal($scope.subtotal);
-
-    }
-
-    $scope.grandTotal = function(total) {
-      $scope.total += total;
-
-      console.log("grandTotal");
-    }
-
-    $scope.getQuote = function() {
-      $http.get('stocks').success(function(response) {
-        console.log(response);
-      });
-    }
-
-    $scope.buyStock = function() {
-
-    }
-
-    $scope.loadInvestor();
-    $scope.loadStocks();
-  }]) // end InvestorCtrl
+  ]) // end InvestorCtrl
 
 
 
